a137991ff129661adcf3ec00e1cdb4b79919c14e
plthompson <patrickthompson9@gmail.com>
2016-04-26 17:50:23 -0600

    Added nestedness

--- ui.R
+++ ui.R
@@ -4,11 +4,11 @@ shinyUI(fluidPage(
   fluidRow(
     column(3,
            wellPanel(
-             selectInput("response","Network measure",choices=list("Link density" = "LD", "Species (nodes)" = "N","Links" = "Ltot","Connectance" = "C", "Trophic levels" = "Trophic_levels"),selected = "LD"),
+             selectInput("response","Network measure",choices=list("Link density" = "LD", "Species (nodes)" = "N","Links" = "Ltot","Connectance" = "C", "Nestedness" = "Nestedness","Compartmentalization" = "Cbar","Trophic levels" = "Trophic_levels"),selected = "LD"),
              selectInput("com_type", "Community",choices = list("Food web" = 3,"Competitive" = 1, "Mixed" = 2),selected = 3),
              selectInput("disp_select","Dispersal rate",choices = list("Low - 0.001" = 1,"Intermediate - 0.01" = 2, "High - 0.05" = 3),selected = 2),
-             numericInput("i_patch","Initial patch (pixel with white outline)",value = 85,min = 51,max=150),
-             radioButtons("contrast","Contrast (pixel with black outline)",choices = list("Same patch"= 0,"Same climate" = 50),selected = 50),
+             numericInput("i_patch","Initial patch (pixel with black outline)",value = 85,min = 51,max=150),
+             radioButtons("contrast","Contrast (pixel with white outline)",choices = list("Same patch"= 0,"Same climate" = 50),selected = 50),
              sliderInput("f_time","Contrast time",min = 2000,max = 7000,step = 200,value = 7000,round=T,animate = T),
              "press play button (above right) to animate through time"
              
